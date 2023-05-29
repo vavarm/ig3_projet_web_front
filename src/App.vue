cfff73e29d10e54c1144af5b911776decdc0b2d4
vavarm <v.racaud-minuzzi@orange.fr>
2023-05-29 17:11:55 +0200

    feat: add drawer to navbar on small screens

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,11 @@ export default {
   components: {
     Navbar,
   },
+  data() {
+    return {
+      drawer: false,
+    }
+  },
   created() {
     // get user info from cookies
     const cookies = document.cookie.split(";")
@@ -24,11 +29,99 @@ export default {
       this.$store.commit("setUserAdminLevel", user_admin_level)
     }
   },
+  methods: {
+    handleDrawer() {
+      this.drawer = !this.drawer
+      console.log(this.drawer)
+    },
+    logout() {
+      this.drawer = false
+      this.$store.commit("logout")
+      this.$router.push("/")
+    },
+    goToHome() {
+      this.drawer = false
+      this.$router.push("/")
+    },
+    goToLessons() {
+      this.drawer = false
+      this.$router.push("/lessons")
+    },
+    goToEvents() {
+      this.drawer = false
+      this.$router.push("/events")
+    },
+    goToMyEvents() {
+      this.drawer = false
+      this.$router.push("/myevents")
+    },
+    goToUsers() {
+      this.drawer = false
+      this.$router.push("/users")
+    },
+    goToAccount() {
+      this.drawer = false
+      this.$router.push("/account")
+    },
+    goToLogin() {
+      this.drawer = false
+      this.$router.push("/login")
+    },
+    goToSignup() {
+      this.drawer = false
+      this.$router.push("/signup")
+    },
+  },
+  computed: {
+    isLoggedIn() {
+      return this.$store.getters.isLoggedIn
+    },
+    isAdmin() {
+      return this.$store.getters.isAdmin
+    },
+  },
 }
 </script>
 
 <template>
-    <Navbar class="navbar" />
+    <Navbar class="navbar" @drawer="handleDrawer"/>
+    <div v-if="drawer" class="drawer">
+      <v-btn @click="goToHome" variant="text" width="100%" height="10%">
+        <v-icon>mdi-home</v-icon>
+        <span>Home</span>
+      </v-btn>
+      <div class="divider"></div>
+      <v-btn @click="goToLessons" variant="text" width="100%" height="10%">
+        <v-icon>mdi-book</v-icon>
+        <span>Lessons</span>
+      </v-btn>
+      <div v-if="isLoggedIn" class="divider"></div>
+      <v-btn v-if="isLoggedIn" @click="goToEvents" variant="text" width="100%" height="10%">
+        <v-icon>mdi-calendar</v-icon>
+        <span>Events</span>
+      </v-btn>
+      <div v-if="isLoggedIn" class="divider"></div>
+      <v-btn v-if="isLoggedIn" @click="goToMyEvents" variant="text" width="100%" height="10%">
+        <v-icon>mdi-calendar-check</v-icon>
+        <span>My Events</span>
+      </v-btn>
+      <div v-if="isAdmin" class="divider"></div>
+      <v-btn v-if="isAdmin" @click="goToUsers" variant="text" width="100%" height="10%">
+        <v-icon>mdi-account-group</v-icon>
+        <span>Users</span>
+      </v-btn>
+      <div v-if="isLoggedIn" class="divider"></div>
+      <v-btn v-if="isLoggedIn" @click="goToAccount" variant="text" width="100%" height="10%">
+        <v-icon>mdi-account</v-icon>
+        <span>Account</span>
+      </v-btn>
+      <div v-if="!isLoggedIn" class="divider"></div>
+      <v-btn color="primary" @click="goToLogin" v-if="!isLoggedIn" width="100%" height="10%">Login</v-btn>
+      <div v-if="!isLoggedIn" class="divider"></div>
+      <v-btn color="primary" @click="goToSignup" v-if="!isLoggedIn" width="100%" height="10%">Sign Up</v-btn>
+      <div v-if="isLoggedIn" class="divider"></div>
+      <v-btn color="red" @click="logout" v-if="isLoggedIn" variant="text" width="100%" height="10%">Logout</v-btn>
+    </div>
     <router-view></router-view>
 </template>
 
@@ -41,6 +134,23 @@ export default {
   background-color: #fff;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   padding: 10px;
+}
+.drawer {
+  position: absolute;
+  z-index: 100;
+  top: 60px;
+  background-color: lightgray;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  height: calc(100vh - 60px);
+  width: 100%;
+}
+.divider {
+  width: 100%;
+  height: 2px;
+  background-color: black;
 }
 .home-section {
   flex: 1;
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { useDisplay } from "vuetify"
-const { lgAndUp } = useDisplay()
+const { lgAndUp, smAndDown } = useDisplay()
 </script>
 
 <script>
@@ -10,6 +10,9 @@ export default {
     logout() {
       this.$store.commit("logout")
       this.$router.push("/")
+    },
+    drawer() {
+      this.$emit("drawer")
     },
   },
   computed: {
@@ -27,55 +30,58 @@ export default {
   <div class="navbar">
     <v-toolbar-title class="ml-2">
       <v-icon class="mr-2">mdi-school</v-icon>
-      <span v-if="lgAndUp">Learn'nTeach</span>
+      <span>Learn'nTeach</span>
     </v-toolbar-title>
     <v-spacer />
     <v-toolbar-items>
-      <v-btn to="/">
+      <v-btn v-if="!smAndDown" to="/">
         <v-icon>mdi-home</v-icon>
         <span v-if="lgAndUp" class="ml-1">Home</span>
         <v-tooltip activator="parent" location="bottom">
           Home
         </v-tooltip>
       </v-btn>
-      <v-btn to="/lessons">
+      <v-btn v-if="!smAndDown" to="/lessons">
         <v-icon>mdi-book</v-icon>
         <span v-if="lgAndUp" class="ml-1">Lessons</span>
         <v-tooltip activator="parent" location="bottom">
           Lessons
         </v-tooltip>
       </v-btn>
-      <v-btn v-if="isLoggedIn" to="/events">
+      <v-btn v-if="isLoggedIn && !smAndDown" to="/events">
         <v-icon>mdi-calendar</v-icon>
         <span v-if="lgAndUp" class="ml-1">Events</span>
         <v-tooltip activator="parent" location="bottom">
           Events
         </v-tooltip>
       </v-btn>
-      <v-btn v-if="isLoggedIn" to="/myevents">
+      <v-btn v-if="isLoggedIn && !smAndDown" to="/myevents">
         <v-icon>mdi-calendar-check</v-icon>
         <span v-if="lgAndUp" class="ml-1">My Events</span>
         <v-tooltip activator="parent" location="bottom">
           My Events
         </v-tooltip>
       </v-btn>
-      <v-btn v-if="isAdmin" to="/users">
+      <v-btn v-if="isAdmin && !smAndDown" to="/users">
         <v-icon>mdi-account-group</v-icon>
         <span v-if="lgAndUp" class="ml-1">Users</span>
         <v-tooltip activator="parent" location="bottom">
           Users
         </v-tooltip>
       </v-btn>
-        <v-btn v-if="isLoggedIn" to="/account">
+        <v-btn v-if="isLoggedIn && !smAndDown" to="/account">
         <v-icon>mdi-account</v-icon>
         <span v-if="lgAndUp" class="ml-1">Account</span>
         <v-tooltip activator="parent" location="bottom">
           {{ this.$store.getters.getMailAddress }}
         </v-tooltip>
       </v-btn>
-      <v-btn color="primary" class="ml-5" rounded="xl" to="/login" v-if="!isLoggedIn">Login</v-btn>
-      <v-btn color="primary" class="ml-2" rounded="xl" to="/signup" v-if="!isLoggedIn">Sign Up</v-btn>
-      <v-btn text color="red" class="ml-5" @click="logout" v-if="isLoggedIn">Logout</v-btn>
+      <v-btn color="primary" class="ml-5" rounded="xl" to="/login" v-if="!isLoggedIn && !smAndDown">Login</v-btn>
+      <v-btn color="primary" class="ml-2" rounded="xl" to="/signup" v-if="!isLoggedIn && !smAndDown">Sign Up</v-btn>
+      <v-btn text color="red" class="ml-5" @click="logout" v-if="isLoggedIn && !smAndDown">Logout</v-btn>
+      <v-btn icon v-if="smAndDown" @click="drawer">
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
     </v-toolbar-items>
   </div>
 </template>
